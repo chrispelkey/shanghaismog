0b9c7d8035fb96b3a606a5e8ec80318c095b365b
Chris <chris.pelkey@gmail.com>
2018-01-05 12:10:52 -0600

    Update Smog Time Series.R

--- Smog Time Series.R
+++ Smog Time Series.R
@@ -1,25 +1,33 @@
 library(readr)
 library(imputeTS)
 library(xts)
+library(zoo)
+library(timeSeries)
+library(astsa)
 
 # Read in the data
-ts_df <- Beijing_2008_HourlyPM2_5_created20140325 <- read_csv("C:/Users/cpelkey/Desktop/Data/Beijing_2008_HourlyPM2.5_created20140325.csv")
+ts_df <- read_csv("C:/Users/cpelkey/Desktop/Data/Smog/Smog/Smog.csv")
 
-# Convert to xts data
-Date <- xts(ts_df, order.by = as.Date(as.character('Date (LST)')),
-                  format="%m/%d/%Y %H:%M")
+# Convert to date format
+DateTS <- as.POSIXct(strptime(ts_df$Date,
+                  "%m/%d/%Y %H:%M"))
 
-cbind(ts_df, Date)
+ts_df$Date <- DateTS
+
+# Remove extraneous columns and convert NAs
+ts_df[ts_df == -999] <- NA
 ts_df <- subset(ts_df, select=c('Date', 'Value'))
-ts_df <- as.xts(x = ts_df)
-indexClass(convertIndex(ts_df$Date, 'POSIXct'))
+ts_df <- removeNA(ts_df)
+ts_df <- as.xts(ts_df$Value, order.by = ts_df$Date)
 
 # Fix the missing points in time using exponential smoothing
-lappply(ts_df, function(x){ts_df[x] == -999 <- NA; x})
+na.locf(ts_df, na.rm = FALSE, fromLast = FALSE)
 
-ts_df[ts_df == -999] <- NA
-
-na.ma(ts_df$Value, k = 4, weighting = "exponential")
+# Subset for two years of data
+subset <- ts_df[53356:75098]
 
 # Review graphs of the series to detemine stationarity, seasonality, differencing
-plot(ts_df)
+
+
+plot(subset)
+acf2(subset, max.lag = 60)
