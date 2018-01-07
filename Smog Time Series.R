630b209c89e68cf5709025d6c57ee418407b7292
Chris <chris.pelkey@gmail.com>
2018-01-07 17:49:17 -0600

    Update Smog Time Series.R
    Working on converting time series to include missing dates using zoo object

--- Smog Time Series.R
+++ Smog Time Series.R
@@ -6,19 +6,25 @@ library(timeSeries)
 library(astsa)
 
 # Read in the data
-ts_df <- read_csv("C:/Users/cpelkey/Desktop/Data/Smog/Smog/Smog.csv")
+# ts_df <- read_csv("C:/Users/cpelkey/Desktop/Data/Smog/Smog/Smog.csv")
+ts_df <- read_csv("~/shanghaismog/Beijing_2008_HourlyPM2.5_created20140325.csv")
+ts_df <- as.data.frame(ts_df)
 
 # Convert to date format
-DateTS <- as.POSIXct(strptime(ts_df$Date,
-                  "%m/%d/%Y %H:%M"))
+ts_df$Date <- as.POSIXct(strptime(ts_df$Date, format = "%m/%d/%Y %H:%M"))
 
-ts_df$Date <- DateTS
+# Add in missing times from the time series
+temp.zoo <- zoo(x = ts_df[,2], order.by = ts_df[,1])
+ts_df <- merge(temp.zoo, zoo(, seq(start(temp.zoo), end(temp.zoo), by = "min")), all = TRUE)
 
 # Remove extraneous columns and convert NAs
 ts_df[ts_df == -999] <- NA
 ts_df <- subset(ts_df, select=c('Date', 'Value'))
-ts_df <- removeNA(ts_df)
+
+
+# ts_df <- removeNA(ts_df)
 ts_df <- as.xts(ts_df$Value, order.by = ts_df$Date)
+periodicity(ts_df)
 
 # Fix the missing points in time using exponential smoothing
 na.locf(ts_df, na.rm = FALSE, fromLast = FALSE)
